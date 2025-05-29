96b25cfca1cd5b7059162c25eb68909247ebe4d0
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-05-29 11:00:27 -0300

    feat: alterar função de leitura de CSV para read_delim e ajustar delimitador

--- analisedados/captura-cnpj/src/r/temp.R
+++ analisedados/captura-cnpj/src/r/temp.R
@@ -1,8 +1,9 @@
 # install.packages("tidyverse")
 
 library(tidyverse)
-base <- read_csv(
+
+base <- read_delim(
   file = "C:/Users/cesar.oliveira/github/projetos/analisedados/captura-cnpj/data/processed/cnpjs_data.csv",
-  sep = ";",
-  col_types = list(col_character())
+  col_types = list(col_character()),
+  delim = ";"
 )
