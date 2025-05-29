7054d197d5469b74cd67f6cdda93d730a450c587
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-05-29 10:56:40 -0300

    feat: remover arquivo extra.R e ajustar caminho do arquivo CSV em temp.R

--- analisedados/captura-cnpj/src/r/extra.R
+++
@@ -1,5 +0,0 @@
-
-
-dplyr::tibble(cnpj_dispendio = as.integer(BASE$cnpj_dispendio)) |>
- dplyr::count(cnpj_dispendio)
-
--- analisedados/captura-cnpj/src/r/temp.R
+++ analisedados/captura-cnpj/src/r/temp.R
@@ -1,4 +1,8 @@
-library(tidyverse)
-base <- read_csv("./data/processed/cnpjs_data.csv", sep = ";")
+# install.packages("tidyverse")
 
-View(base)
+library(tidyverse)
+base <- read_csv(
+  file = "C:/Users/cesar.oliveira/github/projetos/analisedados/captura-cnpj/data/processed/cnpjs_data.csv",
+  sep = ";",
+  col_types = list(col_character())
+)
