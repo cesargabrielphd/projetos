d4cf6d24abaa3102b0cc9a2f5f7b2726bab156a1
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-05-29 11:50:51 -0300

    Ignorar todas as pastas e arquivos em **/data/

--- analisedados/captura-cnpj/src/r/manipul/join_base_and_request.R
+++ analisedados/captura-cnpj/src/r/manipul/join_base_and_request.R
@@ -6,11 +6,16 @@
 # > dplyr::full_join(x = BASE, y = base, by = c("coluna_x" = "coluna_y"))
 ################################################################################
 # referencias:
+# - https://www.youtube.com/watch?v=xnUo25VRH70
 # - https://statisticsglobe.com/r-dplyr-join-inner-left-right-full-semi-anti
 ################################################################################
 
 # SET UP PACKAGES ----
 use("dplyr", c("full_join"))
+
+# CARREGAR BASES ----
+source("src/r/read/read_base_mcti.R")
+source("src/r/read/read_request.R")
 
 # JUNTAR BASES ----
 base_mcti_request <- dplyr::full_join(
@@ -19,4 +24,9 @@ base_mcti_request <- dplyr::full_join(
   by = "cnpj"
 )
 
-view(base_mcti_request)
+# SALVANDO BASE COMPLETA ----
+write.csv(
+  x = base_mcti_request,
+  file = "data/processed/base_mcti_request.csv",
+  row.names = FALSE
+)
